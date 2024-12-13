6a480949ec18b38f38778158804bfb44229caec6
littond <littond@msoe.edu>
2024-12-12 22:29:36 -0600

    commented and removed unnecessary files

--- src/main/java/com/mongodb/quickstart/Read.java
+++ src/main/java/com/mongodb/quickstart/Read.java
@@ -31,64 +31,16 @@ public class Read {
             categories = finalDB.getCollection("Dim_Categories");
             directors = finalDB.getCollection("Dim_Directors");
 
-            //query1();
             //query3();
             //query4();
             //query5();
-            query9();
-
-            /* examples:
-            // find one document with Filters.eq()
-            Document student2 = gradesCollection.find(eq("student_id", 10000)).first();
-            System.out.println("Student 2: " + student2.toJson());
-
-            // find a list of documents and use a List object instead of an iterator
-            List<Document> studentList = gradesCollection.find(gte("student_id", 10000)).into(new ArrayList<>());
-            System.out.println("Student list with an ArrayList:");
-            for (Document student : studentList) {
-                System.out.println(student.toJson());
-            }
-             */
+            //query6();
+            //query7();
+            //query8();
+            //query9();
         }
     }
 
-    private static void query1() {
-        // Perform Aggregation
-        AggregateIterable<Document> result = recordings.aggregate(Arrays.asList(
-                // Step 1: Join with fact_collection
-                Aggregates.lookup(
-                        "Fact_Collection",  // Foreign collection
-                        "_id",              // Local field in dim_recordings
-                        "recording_id",     // Foreign field in fact_collection
-                        "facts"             // Resulting field
-                ),
-                // Step 2: Unwind the facts array NOTE FROM DAVID: not really sure what this does
-                Aggregates.unwind("$facts", new UnwindOptions().preserveNullAndEmptyArrays(false)),
-                // Step 3: Join with dim_category
-                Aggregates.lookup(
-                        "Dim_Categories",     // Foreign collection
-                        "facts.category_id", // Field in facts
-                        "_id",              // Field in dim_category
-                        "categories"        // Resulting field
-                ),
-                // Step 4: create a new field called category_name and set it to the category
-                Aggregates.addFields(Collections.singletonList(
-                        new Field<>("categories_name", "$categories.name")
-                )),
-                // Step 5: Remove the fact and category fields to simplify output
-                Aggregates.project(Projections.exclude("facts", "categories")) // Remove facts if still present
-        ));
-
-
-        // Print results
-        int length = 0;
-        for (Document doc : result) {
-            System.out.println(doc.toJson());
-            length++;
-        }
-        System.out.println("number of results: " + length);
-
-    }
     /*
     List the number of videos for each video category.
      */
@@ -164,29 +116,27 @@ public class Read {
     private static void query5(){
         // Perform Aggregation
         AggregateIterable<Document> result = actors.aggregate(Arrays.asList(
-                // Step 1: Join dim_actors with fact_collection
+                // Join dim_actors with fact_collection
                 Aggregates.lookup(
-                        "Fact_Collection",  // Foreign collection
-                        "_id",              // Local field in dim_actors
-                        "actor_id",         // Foreign field in fact_collection
-                        "facts"             // Resulting field
+                        "Fact_Collection",
+                        "_id",
+                        "actor_id",
+                        "facts"
                 ),
-                // Step 2: Unwind the facts array
+                // Unwind the facts array
                 Aggregates.unwind("$facts", new UnwindOptions().preserveNullAndEmptyArrays(false)),
-                // Step 3: Join fact_collection with dim_category
+                // Join fact_collection with dim_category
                 Aggregates.lookup(
-                        "Dim_Categories",         // Foreign collection
-                        "facts.category_id",    // Field in facts
-                        "_id",                  // Field in dim_category
-                        "categories"            // Resulting field
+                        "Dim_Categories",
+                        "facts.category_id",
+                        "_id",
+                        "categories"
                 ),
-                // Step 4: Group by actor with categories
+                // Group by actor with categories
                 Aggregates.group(
-                        "$name",  // Group by actor name
+                        "$name",
                         Accumulators.addToSet("categories", "$categories.name")
                 )
-
-
         ));
 
         // Print results
@@ -199,33 +149,30 @@ public class Read {
     Which actors have appeared in movies in different video categories?
      */
     private static void query6(){
-        // Perform Aggregation
         AggregateIterable<Document> result = actors.aggregate(Arrays.asList(
-                // Step 1: Join dim_actors with fact_collection
+                // Join dim_actors with fact_collection
                 Aggregates.lookup(
-                        "Fact_Collection",  // Foreign collection
-                        "_id",              // Local field in dim_actors
-                        "actor_id",         // Foreign field in fact_collection
-                        "facts"             // Resulting field
+                        "Fact_Collection",
+                        "_id",
+                        "actor_id",
+                        "facts"
                 ),
-                // Step 2: Unwind the facts array
+                // Unwind the facts array
                 Aggregates.unwind("$facts", new UnwindOptions().preserveNullAndEmptyArrays(false)),
-                // Step 3: Join fact_collection with dim_category
+                // Join fact_collection with dim_category
                 Aggregates.lookup(
                         "Dim_Categories",         // Foreign collection
-                        "facts.category_id",    // Field in facts
-                        "_id",                  // Field in dim_category
-                        "categories"            // Resulting field
+                        "facts.category_id",
+                        "_id",
+                        "categories"
                 ),
-                // Step 4: Group by actor with categories
+                // Group by actor with categories
                 Aggregates.group(
-                        "$name",  // Group by actor name
+                        "$name",
                         Accumulators.addToSet("categories", "$categories.name")
                 ),
-                // Step 5: Filter actors with multiple distinct categories
+                // Filter actors with multiple distinct categories
                 Aggregates.match(Filters.expr(new Document("$gt", Arrays.asList(new Document("$size", "$categories"), 1))))
-
-
         ));
 
         // Print results
@@ -238,32 +185,31 @@ public class Read {
     Which actors have not appeared in a comedy?
      */
     private static void query7(){
-        // Perform Aggregation
         AggregateIterable<Document> result = actors.aggregate(Arrays.asList(
-                // Step 1: Join dim_actors with fact_collection
+                // Join dim_actors with fact_collection
                 Aggregates.lookup(
-                        "Fact_Collection",  // Foreign collection
-                        "_id",              // Local field in dim_actors
-                        "actor_id",         // Foreign field in fact_collection
-                        "facts"             // Resulting field
+                        "Fact_Collection",
+                        "_id",
+                        "actor_id",
+                        "facts"
                 ),
-                // Step 2: Unwind the facts array
+                // Unwind the facts array
                 Aggregates.unwind("$facts", new UnwindOptions().preserveNullAndEmptyArrays(false)),
-                // Step 3: Join fact_collection with dim_category
+                // Join fact_collection with dim_category
                 Aggregates.lookup(
-                        "Dim_Categories",    // Foreign collection
-                        "facts.category_id", // Field in facts
-                        "_id",               // Field in dim_category
-                        "categories"         // Resulting field
+                        "Dim_Categories",
+                        "facts.category_id",
+                        "_id",
+                        "categories"
                 ),
-                // Step 4: Unwind categories array to flatten structure
+                // Unwind categories array to flatten structure
                 Aggregates.unwind("$categories", new UnwindOptions().preserveNullAndEmptyArrays(false)),
-                // Step 5: Group by actor with distinct categories
+                // Group by actor with distinct categories
                 Aggregates.group(
-                        "$name", // Group by actor name
+                        "$name",
                         Accumulators.addToSet("categories", "$categories.name")
                 ),
-                // Step 6: Filter out actors who have been in the "Comedy" category
+                // Filter out actors who have been in the "Comedy" category
                 Aggregates.match(Filters.not(Filters.in("categories", Arrays.asList("Comedy"))))
         ));
 
@@ -277,32 +223,31 @@ public class Read {
     Which actors have appeared in both a comedy and an action adventure movie?
      */
     private static void query8(){
-        // Perform Aggregation
         AggregateIterable<Document> result = actors.aggregate(Arrays.asList(
-                // Step 1: Join dim_actors with fact_collection
+                // Join dim_actors with fact_collection
                 Aggregates.lookup(
-                        "Fact_Collection",  // Foreign collection
-                        "_id",              // Local field in dim_actors
-                        "actor_id",         // Foreign field in fact_collection
-                        "facts"             // Resulting field
+                        "Fact_Collection",
+                        "_id",
+                        "actor_id",
+                        "facts"
                 ),
-                // Step 2: Unwind the facts array
+                // Unwind the facts array
                 Aggregates.unwind("$facts", new UnwindOptions().preserveNullAndEmptyArrays(false)),
-                // Step 3: Join fact_collection with dim_category
+                // Join fact_collection with dim_category
                 Aggregates.lookup(
-                        "Dim_Categories",    // Foreign collection
-                        "facts.category_id", // Field in facts
-                        "_id",               // Field in dim_category
-                        "categories"         // Resulting field
+                        "Dim_Categories",
+                        "facts.category_id",
+                        "_id",
+                        "categories"
                 ),
-                // Step 4: Unwind categories array to flatten structure
+                // Unwind categories array to flatten structure
                 Aggregates.unwind("$categories", new UnwindOptions().preserveNullAndEmptyArrays(false)),
-                // Step 5: Group by actor with distinct categories
+                // Group by actor with distinct categories
                 Aggregates.group(
-                        "$name", // Group by actor name
+                        "$name",
                         Accumulators.addToSet("categories", "$categories.name")
                 ),
-                // Step 6: Filter actors who have both "Comedy" and "Action Adventure" in their categories
+                // Filter actors who have both "Comedy" and "Action & Adventure" in their categories
                 Aggregates.match(Filters.and(
                         Filters.in("categories", Arrays.asList("Comedy")),
                         Filters.in("categories", Arrays.asList("Action & Adventure"))
